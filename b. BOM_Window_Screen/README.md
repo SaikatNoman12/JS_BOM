1a4b6207da5869cc4a84ae2db7549070a3039e4a
saikatnoman12 <noommaa240@gmail.com>
2022-10-09 14:15:57 +0600

    add BOM window object screen

---
+++ b. BOM_Window_Screen/README.md
@@ -0,0 +1,17 @@
+# JavaScript Widow Object Screen
+
+### Window Object Screen Property:
+
+    screen.weight;
+
+    screen.height;
+
+    screen.availWeight;
+
+    screen.availHeight;
+
+    screen.colorDepth;
+
+    screen.pixelDepth;
+
+    
---
+++ b. BOM_Window_Screen/app.js
@@ -0,0 +1,33 @@
+/* ===== JavaScript DOM Screen ===== */
+
+// use BOM screen.width property:---------
+const demo1 = document.querySelector('.demo1');
+demo1.innerHTML = 'screen.width ' + screen.width;
+
+
+// use BOM screen.height property:---------
+const demo2 = document.querySelector('.demo2');
+demo2.innerHTML = 'screen.height ' + screen.height;
+
+
+// use BOM screen.availWidth property:---------
+const demo3 = document.querySelector('.demo3');
+demo3.innerHTML = 'screen.availWidth ' + screen.availWidth;
+
+
+// use BOM screen.availHeight property:---------
+const demo4 = document.querySelector('.demo4');
+demo4.innerHTML = 'screen.availHeight ' + screen.availHeight;
+
+
+// use BOM screen.colorDepth property:---------
+const demo5 = document.querySelector('.demo5');
+demo5.innerHTML = 'screen.colorDepth ' + screen.colorDepth;
+
+
+// use BOM screen.pixelDepth property:---------
+const demo6 = document.querySelector('.demo6');
+demo6.innerHTML = 'screen.pixelDepth ' + screen.pixelDepth;
+
+
+
