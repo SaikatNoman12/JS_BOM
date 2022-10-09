6226fe49e569b4ec13694f6b79d55c5d9139608f
saikatnoman12 <noommaa240@gmail.com>
2022-10-10 00:19:36 +0600

    add BOM window navigation

---
+++ e. BOM_Window_Navigator/README.md
@@ -0,0 +1,27 @@
+# JavaScript BOM Window Navigator Object
+
+### window navigator object property:
+
+    navigator.appName;
+
+    navigator.appCodeName;
+
+    navigator.platform;
+
+    navigator.cookieEnabled;
+
+    navigator.product;
+
+    navigator.appVersion;
+
+    navigator.userAgent;
+
+    navigator.onLine;
+
+    navigator.language;
+
+    navigator.languages;
+
+### window navigator object method: 
+
+    navigator.javaEnabled();
---
+++ e. BOM_Window_Navigator/app.js
@@ -0,0 +1,60 @@
+/* ==== JavaScript BOM Window Navigator Object ==== */
+
+
+// use window.navigator.appName property:---------
+const demo1 = document.getElementById('navigator');
+demo1.innerHTML = 'window.navigator.appName:- ' + navigator.appName;
+
+
+// use window.navigator.appCodeName property:---------
+const demo2 = document.getElementById('app-code-name');
+demo2.innerHTML = 'window.navigator.appCodeName:- ' + navigator.appCodeName;
+
+
+// use window.navigator.platform property:---------
+const demo3 = document.getElementById('platform');
+demo3.innerHTML = 'window.navigator.platform:- ' + navigator.platform;
+
+
+// use window.navigator.cookieEnabled property:---------
+const demo4 = document.getElementById('cookie-enabled');
+demo4.innerHTML = 'window.navigator.cookieEnabled:- ' + navigator.cookieEnabled;
+
+
+// use window.navigator.product property:---------
+const demo5 = document.getElementById('product');
+demo5.innerHTML = 'window.navigator.product:- ' + navigator.product;
+
+
+// use window.navigator.appVersion property:---------
+const demo6 = document.getElementById('app-version');
+demo6.innerHTML = 'window.navigator.appVersion:- ' + navigator.appVersion;
+
+
+// use window.navigator.userAgent property:---------
+const demo7 = document.getElementById('user-agent');
+demo7.innerHTML = 'window.navigator.userAgent:- ' + navigator.userAgent;
+
+
+// use window.navigator.onLine property:---------
+const demo8 = document.getElementById('online');
+demo8.innerHTML = 'window.navigator.onLine:- ' + navigator.onLine;
+
+
+// use window.navigator.language property:---
+const demo9 = document.getElementById('language');
+demo9.innerHTML = 'window.navigator.language:- ' + navigator.language;
+
+
+// use window.navigator.languages property:---
+const demo10 = document.getElementById('languages');
+demo10.innerHTML = 'window.navigator.languages:- ' + navigator.languages;
+
+
+// use window.navigator.javaEnabled() property:---
+const demo11 = document.getElementById('java-enabled');
+demo11.innerHTML = 'window.navigator.javaEnabled():- ' + navigator.javaEnabled();
+
+
+
+
